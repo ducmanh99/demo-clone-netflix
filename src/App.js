6c918ce602c748428cf2806ebd3491fd5aa80aa2
ducmanh99 <pdm241099@gmail.com>
2022-08-06 14:53:53 +0700

    file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Content from "./components/Content/Content";
-import Intro from "./components/Intro/Intro";
-import MoviesDetail from "./components/MoviesDetail/MoviesDetail";
+// import Content from "./components/Content/Content";
+// import Intro from "./components/Intro/Intro";
+// import MoviesDetail from "./components/MoviesDetail/MoviesDetail";
 import { useSelector } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
-import SearchMovies from "./components/SearchMovies/SearchMovies";
+// import SearchMovies from "./components/SearchMovies/SearchMovies";
 import Home from "./components/Pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./components/Pages/Search";
@@ -17,6 +17,7 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route exact path="/" element={<Home />} />
+					<Route path="/demo-clone-netflix/" element={<Home />} />
 					<Route path="/search" element={<Search />} />
 				</Routes>
 			</BrowserRouter>
